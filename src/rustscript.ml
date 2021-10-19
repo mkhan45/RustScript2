8cdfbd42e37dc8b8a246b9498c40d9516884eb8e
Mikail Khan <mikail@mikail-khan.com>
2021-10-19 12:34:59 -0400

    added repl/cli

--- src/rustscript.ml
+++ src/rustscript.ml
@@ -7,17 +7,37 @@ let eval state s =
     let eval_closure = Eval.eval_expr parsed in
     eval_closure state
 
+let rec repl state = 
+    printf "> ";
+    Out_channel.flush stdout;
+    match In_channel.input_line ~fix_win_eol:true stdin with
+    | Some "\n" -> ()
+    | None -> ()
+    | Some line -> 
+        match eval state line with
+            | (Unit, new_state) -> repl new_state
+            | (evaled, new_state) ->
+                    printf "%s\n" (string_of_val evaled);
+                    Out_channel.flush stdout;
+                    repl new_state
+
+let run_line state line =
+    match eval state line with
+        | (Unit, new_state) -> new_state
+        | (evaled, new_state) ->
+            printf "%s\n" (string_of_val evaled);
+            Out_channel.flush stdout;
+            new_state
+
 let () =
-    let state: state = Map.empty (module String) in
     let args = Sys.get_argv () in
+    let state: state = Map.empty (module String) in
     match args |> Array.to_list with
-        | _::exprs -> 
-                let _ = List.fold_left 
-                ~init:state
-                ~f:(fun state expr -> 
-                        let (result, state) = eval state expr in
-                        result |> string_of_val |> printf "%s\n";
-                        state) 
-                exprs
+        | [_; filename] ->
+                let in_stream = In_channel.create filename in
+                let _ = In_channel.fold_lines in_stream ~fix_win_eol:true ~init:state ~f:run_line
                 in ()
-        | _ -> print_endline "Usage: {cmd} [expression]"
+        | [_] ->
+            repl state
+        | _ ->
+                printf "Usage: 'rustscript <filename>' or just 'rustscript' for REPL\n"
