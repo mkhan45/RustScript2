ab5100b53d57d3643aa8d624d567d0f64a4909c5
Mikail Khan <mikail@mikail-khan.com>
2021-11-13 01:22:55 -0500

    fixed repl, closes #29

--- bin/rustscript_cli.ml
+++ bin/rustscript_cli.ml
@@ -3,20 +3,19 @@ open Rustscript.Types
 open Base
 open Stdio
 
-let rec repl state = 
+let rec repl state ss = 
     printf "> ";
     Out_channel.flush stdout;
-    let ss = {static_atoms = []; static_idents = []; static_block_funcs = []; call_stack = []} in
     match In_channel.input_line ~fix_win_eol:true stdin with
     | Some "\n" -> ()
     | None -> ()
     | Some line -> 
         match Rustscript.Run.eval ss state line with
-            | (Tuple [], new_state), _ss -> repl new_state
-            | (evaled, new_state), _ss ->
+            | (Tuple [], new_state), ss -> repl new_state ss
+            | (evaled, new_state), ss ->
                     printf "%s\n" (Rustscript.Types.string_of_val ss evaled);
                     Out_channel.flush stdout;
-                    repl new_state
+                    repl new_state ss
 
 let () =
     let args = Sys.get_argv () in
@@ -24,6 +23,6 @@ let () =
     match args |> Array.to_list with
         | [_; filename] -> let _state = Run.run_file filename (ss, state) in ()
         | [_] ->
-            repl state
+            repl state ss
         | _ ->
                 printf "Usage: 'rustscript <filename>' or just 'rustscript' for REPL\n"
